a8748b51ca7df5c04469068999cd6517e4b132ee
Alex Bailey <alexwbailey7@gmail.com>
2019-10-31 16:32:27 -0500

    Adding links for buttons

---
+++ portfolio.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+//Profile
+$("#profile_github").on("click", function(){
+    window.open("https://github.com/xKingAlex");
+})
+$("#profile_linkedin").on("click", function(){
+    window.open("https://www.linkedin.com/in/alex-bailey-19280718a/");
+})
+$("#profile_resume").on("click", function(){
+    window.open("https://docs.google.com/document/d/1VEw1mHKJst-J4DFjG7_l4SM-sgEAozeImoHN1AhP4TU/edit?usp=sharing");
+})
+
+
+//Nsearch
+$("#nsearch_github").on("click", function(){
+    window.open("https://github.com/xKingAlex/NSearch");
+})
+$("#nsearch_project").on("click", function(){
+    window.open("https://xkingalex.github.io/NSearch/");
+})
+
+//RPS
+$("#rps_github").on("click", function(){
+    window.open("https://github.com/xKingAlex/RPS-Multiplayer");
+})
+$("#rps_projects").on("click", function(){
+    window.open("https://xkingalex.github.io/RPS-Multiplayer/");
+})
+
+//Word Guess
+$("#wordguess_github").on("click", function(){
+    window.open("https://github.com/xKingAlex/Word-Guess-Game");
+})
+$("#wordguess_project").on("click", function(){
+    window.open("https://xkingalex.github.io/Word-Guess-Game/");
+})
